27b8ee7a93cd2292d4af81e9ffde9295298be76e
Yizheng Huang <huangyz0918@gmail.com>
2022-05-16 17:44:01 +0800

    ✨ feat: added the basic server-client structure

--- alaas/client/client.py
+++ alaas/client/client.py
@@ -1,9 +1,33 @@
+import requests
+
+
 class Client:
-    def __int__(self, server_url):
+    """
+    Client: the active learning client class.
+    """
+
+    def __init__(self, server_url: str):
         self.server_url = server_url
 
     def push(self, data_list, asynchronous=False):
-        pass
+        """
+        push the data url list to the cloud active learning server.
+        """
+        return requests.post(self.server_url + "/push", params={'asynchronous': asynchronous}, json=data_list)
 
     def query(self, budget):
-        pass
+        """
+        start the active learning process on current data pool with the given budget.
+        """
+        return requests.get(self.server_url + "/query", params={'budget': budget})
+
+
+if __name__ == '__main__':
+    with open("/Users/huangyz0918/Desktop/CIFAR-10-images/train.txt") as file:
+        url_list = [line.rstrip() for line in file.readlines()]
+        print(url_list[:5])
+
+        # define the ALaaS client, and push data to the server.
+        client = Client("http://0.0.0.0:8001")
+        print(client.push(data_list=url_list[:5]).text)
+        print(client.query(10).text)
--- alaas/server/server.py
+++ alaas/server/server.py
@@ -1,11 +1,76 @@
-from alaas.server.util import ConfigManager
+import uvicorn
+from typing import List
 
-example_config_path = '/Users/huangyz0918/desktop/zeef/service/example/resnet_triton.yml'
+import os
+from pathlib import Path
+import urllib.request
+from urllib.parse import urlparse
+
+from fastapi import FastAPI
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+
+from alaas.server.util import ConfigManager
+from alaas.server.util import DBManager
+
+EXAMPLE_CONFIG_PATH = '/Users/huangyz0918/desktop/alaas/examples/resnet_triton.yml'
+
+app_server = FastAPI()
+router = InferringRouter()
+
+
+@cbv(router)
+class ALServerModel:
+
+    def __init__(self):
+        self.asynchronous = False
+        self.data_urls = []
+
+    def download_data(self):
+        home_path = str(Path.home())
+        alaas_home = home_path + "/.alaas/"
+        Path(alaas_home).mkdir(parents=True, exist_ok=True)
+        for url in self.data_urls:
+            urllib.request.urlretrieve(url, alaas_home + os.path.basename(urlparse(url).path))
+
+    @router.get("/")
+    def index(self):
+        return {"message": "Welcome to ALaaS Server!"}
+
+    @router.get("/query")
+    def query(self, budget: int):
+        return {"budget": budget}
+
+    @router.post("/push")
+    def push(self, data: List[str], asynchronous: bool):
+        self.data_urls = data
+        self.asynchronous = asynchronous
+        self.download_data()
+        return {'data': data, 'asynchronous': asynchronous}
 
 
 class Server:
-    def __int__(self, config_path):
-        self.cfg_manager = ConfigManager(config_path)
+    """
+    Server: Server Class for Active Learning Services.
+    """
 
-    def start(self):
-        pass
+    def __init__(self, config_path):
+        self.cfg_manager = ConfigManager(config_path)
+        self.db_manager = None
+
+    def start_db(self, db_name, container_name, ports):
+        # TODO: check the port and db name is valid or not, offer some options to start.
+        self.cfg_manager = DBManager(db_name, container_name, ports)
+
+    @staticmethod
+    def start(host="0.0.0.0", port=8000, restful=True):
+        if restful:
+            app_server.include_router(router)
+            uvicorn.run(app_server, host=host, port=port)
+        else:
+            # RPC Server
+            raise NotImplementedError("gRPC server is not available right now.")
+
+
+if __name__ == "__main__":
+    Server(config_path=EXAMPLE_CONFIG_PATH).start(host="0.0.0.0", port=8001)
