4ebf1c1ee03563083b7501b442fcd1b4a27a4e07
Li Yuanming <yli056@e.ntu.edu.sg>
2022-05-29 11:15:32 +0800

    :hammer: Change Config manager to pydantic type

--- alaas/server/util/cfg_manager.py
+++ alaas/server/util/cfg_manager.py
@@ -1,36 +1,42 @@
 """
 Configuration file manager for active learning as a service.
 @author: huangyz0918 (huangyz0918@gmail.com)
+@author: Li Yuanming
 @date: 01/05/2022
 """
 
 import yaml
 
+from alaas.types.models.config import Config
+
 
 class ConfigManager:
 
     def __init__(self, config_path):
-        self.config_dict = ConfigManager.load_config(config_path)
-
-    @property
-    def dry_run(self):
-        return self.config_dict.get('dry_run', False)
+        self.config: Config = ConfigManager.load_config(config_path)
 
     @staticmethod
     def load_config(config_path):
-        result_dict = None
-        with open(config_path, 'r') as stream:
-            try:
-                result_dict = yaml.safe_load(stream)
-            except yaml.YAMLError as exc:
-                print(exc)
-        return result_dict
+        with open(config_path) as f:
+            config_obj = yaml.safe_load(f)
+        return Config.parse_obj(config_obj)
 
-    def get_job_name(self):
-        return self.config_dict['name']
+    @property
+    def name(self):
+        return self.config.name
 
-    def get_job_version(self):
-        return self.config_dict['version']
+    @property
+    def version(self):
+        return self.config.version
 
-    def get_al_config(self):
-        return self.config_dict['active_learning']
+    @property
+    def budget(self):
+        return self.config.active_learning.budget
+
+    @property
+    def strategy(self):
+        return self.config.active_learning.strategy
+
+    @property
+    def al_server(self):
+        return self.config.active_learning.al_server
