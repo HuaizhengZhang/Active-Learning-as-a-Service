84eb21208fd638ec4b8aa79f2e4a8bb028ac66cf
Yizheng Huang <huangyz0918@gmail.com>
2022-06-10 14:16:18 +0800

    🎨 style: clean code

--- alaas/server/serving/triton/client.py
+++ alaas/server/serving/triton/client.py
@@ -33,7 +33,7 @@ class ServeClient:
 
 def triton_inference_func(source_data, batch_size, model_name, address):
     probs = []
-    for batch in np.array_split(source_data, batch_size):
+    for batch in np.array_split(source_data, int(len(source_data)) / batch_size):
         for result in ServeClient().infer(batch, model_name, address=address):
             probs.append(result)
     return probs
@@ -44,17 +44,9 @@ if __name__ == '__main__':
     np.random.seed(0)
     input_data = np.random.rand(*shape).astype(np.float32)
 
-    # client = ServeClient()
-    # outputs_0 = client.infer(input_data, 'resnet', '54.251.31.100:8900')
-
-    # output_1 = client.infer(input_data, 'resnet', '54.251.31.100:8900')
-    outputs_0 = triton_inference_func(input_data, 1, 'resnet', '54.251.31.100:8900')
+    outputs_0 = triton_inference_func(input_data, 2, 'resnet', '54.251.31.100:8900')
     outputs_1 = triton_inference_func(input_data, 4, 'resnet', '54.251.31.100:8900')
-    # print(np.shape(outputs_0))
+
+    print(np.shape(outputs_0), np.shape(outputs_1))
     print(np.amax(outputs_0, axis=0).argsort()[:1], np.shape(outputs_0))
     print(np.amax(outputs_1, axis=0).argsort()[:1], np.shape(outputs_1))
-    # print("\n")
-    # print(np.argpartition(outputs_1, -4)[-4:])
-
-    # input_data = np.array([[0, 1], [0, 2], 2, 3, 4, 5, 6, 7, 8, 9])
-    # print(list(chunks(input_data, 4)))
